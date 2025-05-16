0a1f647b01604af5028faef2b606cd696b1e4117
Denis Leonenko <leograndprix@gmail.com>
2025-05-16 16:51:42 +0300

    Update App.css

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch, useSelector } from 'react-redux'
 
 import positionsReducer from './slices/positionsSlice'
 import filterReducer from './slices/filterSlice'
@@ -9,3 +10,9 @@ export const store = configureStore({
     filter: filterReducer,
   },
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
